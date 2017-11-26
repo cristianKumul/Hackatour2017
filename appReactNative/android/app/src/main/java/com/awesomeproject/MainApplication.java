a28716d661ccac42acfd536402718127a5896b46
German OMEN <gmtz.1991@gmail.com>
2017-11-26 12:07:34 -0500

    updating libraries app

--- appReactNative/android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ appReactNative/android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -9,13 +9,10 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
 import java.util.Arrays;
 import java.util.List;
-
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.appevents.AppEventsLogger;
 
 public class MainApplication extends Application implements ReactApplication {
